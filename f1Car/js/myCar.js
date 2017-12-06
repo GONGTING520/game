50b5b88e5279e2664558bc71547ef34efcc4a33d
GONGTING520 <954160311@qq.com>
2017-12-06 10:40:34 +0800

    删除cover方法，因为cars中已有，修改carBody，在里面添加一张小车图片

--- f1Car/js/myCar.js
+++ f1Car/js/myCar.js
@@ -1,6 +1,6 @@
 var $sum = $('#score span');//获取总分
 function MyCar() {
-    this.carBody = $('<div class="my-car"></div>').css({
+    this.carBody = $('<div class="my-car"><img src="img/F1.png"/></div>').css({
         left: 285,
         bottom: 10
     }).get(0);
@@ -19,26 +19,9 @@ MyCar.prototype.collision = function (arr) {
             if (cover(arr[i].carBody, this.carBody)) {
                 return true;
             }
-        } else {
+        } else {          
             arr.splice(i, 1); //删除此对象
         }
     }
     return false;
 };
-/**
- * 判断自己是否与对向车辆有覆盖部分
- * 
- * @param {object} target 对向车辆
- * @param {object} elem 自己车
- * @returns true表示有覆盖，false表示无覆盖
- */
-function cover(target, elem) {
-    var $targetPos = $(target).offset();
-    var $elemPos = $(elem).offset();
-    if (Math.abs($targetPos.left - $elemPos.left) <= $(elem).width()) {
-        if (Math.abs($targetPos.top - $elemPos.top) <= $(elem).height()) {
-            return true;
-        }
-    }
-    return false;
-}
