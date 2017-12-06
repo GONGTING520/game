0b44549a77d6ab6ac007a50cce77b2ff65e071d5
GONGTING520 <954160311@qq.com>
2017-12-06 11:35:39 +0800

    定义遮罩层的类，需要传一个对象参数来输入遮罩层内容

---
+++ f1Car/js/layout.js
@@ -0,0 +1,22 @@
+/**
+ * 定义遮罩层的类
+ * 
+ */
+function Layout(settings) {
+    this.defaultSettings = {
+        content: ''
+    };
+    $.extend(this.defaultSettings, settings); //将用户设置覆盖到默认设置中
+    this.$mask = $('<div id="mask"></div>');
+    this.$content = $('<div class="mask-content">' + this.defaultSettings.content + '</div>');
+    this.$score = $('<div class="mask-score"></div>');
+    this.$restart = $('<button class="restart">重新开始</button>');
+}
+Layout.prototype.show = function () {
+    this.$score.text('得分：' + $('#score span').text());
+    this.$mask.appendTo(document.body).append(this.$content)
+        .append(this.$score).append(this.$restart); //添加html结构
+    this.$restart.on('click', function () {
+        window.location.reload();
+    });
+}
