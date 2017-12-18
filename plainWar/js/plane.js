3ae0144c9aef7d3583a5b6b70fa8a5460478d665
GONGTING520 <954160311@qq.com>
2017-12-18 20:41:54 +0800

    飞机类，包括我的飞机类和敌人的飞机类，并且可生成一个子弹

---
+++ plainWar/js/plane.js
@@ -0,0 +1,56 @@
+var $container = $('#container');
+var $containerDivs = $('div', $container);
+//飞机类
+function Plane() {
+    this.$planeBody = $('<div></div>');
+    this.aShot = [];
+    switch (Math.round(Math.random() * 2)) {
+        case 0:
+            this.sPosition = 'left';
+            break;
+        case 1:
+            this.sPosition = 'center';
+            break;
+        case 2:
+            this.sPosition = 'right';
+            break;
+    }
+}
+Plane.prototype.shoot = function () {
+    var that = this;
+    // this.shootTimer = setInterval(function () {
+        // if (that.aShot.length < 6) {
+            that.aShot.push(new Shot(that));
+        // }
+    // }, 500);
+};
+
+//敌人的飞机
+function AnemyPlane() {
+    Plane.call(this); //继承飞机类
+    this.$planeBody.addClass('anemy');
+    switch (this.sPosition) {
+        case 'left':
+            this.$planeBody.appendTo($containerDivs[0]);
+            break;
+        case 'center':
+            this.$planeBody.appendTo($containerDivs[1]);
+            break;
+        case 'right':
+            this.$planeBody.appendTo($containerDivs[2]);
+            break;
+    }
+    this.shoot();
+}
+AnemyPlane.prototype = new Plane(); //继承飞机类的方法
+AnemyPlane.prototype.constructor = AnemyPlane; //将构造函数改为自己
+
+//我的飞机
+function MyPlane() {
+    Plane.call(this); //继承飞机类
+    this.$planeBody.addClass('mine').appendTo($containerDivs[1]).css('bottom', 10);
+    this.sPosition = 'center';
+    this.shoot();
+}
+MyPlane.prototype = new Plane(); //继承飞机类的方法
+MyPlane.prototype.constructor = MyPlane; //将构造函数改为自己
