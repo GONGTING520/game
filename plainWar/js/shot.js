e779e8f89fa1d41162b28602d0a814fd6f217063
GONGTING520 <954160311@qq.com>
2017-12-18 20:46:35 +0800

    让生成的子弹动起来

--- plainWar/js/shot.js
+++ plainWar/js/shot.js
@@ -28,4 +28,21 @@ function Shot(owner) {
             this.$shotBody.appendTo($containerDivs[2]);
             break;
     }
+    this.move();
 }
+Shot.prototype.move = function () {
+    //如果所有者是我的飞机
+    if (this.owner.constructor == MyPlane) {
+        this.moveTimer = setInterval(function () {
+            this.$shotBody.css({
+                top: '-=4'
+            });
+        }.bind(this), 50);
+    } else {
+        this.moveTimer = setInterval(function () {
+            this.$shotBody.css({
+                top: '+=4'
+            });
+        }.bind(this), 50);
+    }
+};
